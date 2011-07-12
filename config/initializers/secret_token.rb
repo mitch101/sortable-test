69336735981155f83efa9ebf12dfde1acdef9eb3
Mitch Crowe <mitch.crowe@suite101.com>
2011-07-11 19:49:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SortableTest::Application.config.secret_token = '08a3496a010163d59049453cb57329e5ccfde451a65f71ebc9ed948348ed796dc1c9e2c4b135bd7f2870142f19f970efb3cafde7a0bfe862ea403fe4ef83fe14'
